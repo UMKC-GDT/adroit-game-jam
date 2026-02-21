0e66049ffb542d584ab7898b0c09fecd5e292c9a
Blaine Lockard <blainelockard@gmail.com>
2026-02-20 22:14:09 -0600

    Added Basic Movements

---
+++ Scripts/player.gd
@@ -0,0 +1,73 @@
+extends CharacterBody2D
+
+@export var speed := 40.0
+@export var maxSpeed := 250.0
+@export var airSpeed := 100.0
+@export var groundFriction := 1300.0
+@export var airFriction := 100.0
+@export var jumpVelocity := 430.0
+
+var gravity = -ProjectSettings.get_setting("physics/2d/default_gravity")
+var maxFallSpeed := 1200.0
+
+var inputDir := 0.0
+
+func _process(delta: float) -> void:
+	# check left right input
+	if(Input.is_action_pressed("right")):
+		inputDir = 1.0
+	elif(Input.is_action_pressed("left")):
+		inputDir = -1.0
+	else:
+		inputDir = 0
+		
+	# check all other inputs
+
+
+func _physics_process(delta: float) -> void:
+	if(self.is_on_floor()):
+		doGroundMovement(delta)
+		if(Input.is_action_just_pressed("jump")):
+			self.velocity.y -= jumpVelocity
+	else:
+		doAirMovement(delta)
+	
+	move_and_slide()
+	
+
+func doGroundMovement(delta: float):
+	if(inputDir == 0):
+		# subtract/add friction based on direction of movement
+		if(self.velocity.x > 0):
+			self.velocity.x -= groundFriction * delta
+		elif(self.velocity.x < 0):
+			self.velocity.x += groundFriction * delta
+		
+		if(self.velocity.x < 20 and self.velocity.x > -20):
+			self.velocity.x = 0
+	else:
+		# add to speed
+		self.velocity.x += speed * inputDir
+		# limit speed
+		self.velocity.x = limitSpeed(self.velocity.x, maxSpeed)
+
+func doAirMovement(delta: float):
+	self.velocity.y -= gravity * delta
+	if(self.velocity.y > maxFallSpeed):
+		self.velocity.y = maxFallSpeed
+	
+	if(self.velocity.x > 0):
+		self.velocity.x -= airFriction * delta
+	elif(self.velocity.x < 0):
+		self.velocity.x += airFriction * delta
+	
+	if(inputDir == 1):
+		self.velocity.x += airSpeed * delta
+	elif(inputDir == -1):
+		self.velocity.x -= airSpeed * delta
+
+func limitSpeed(currentSpeed: float, max: float):
+	if(abs(currentSpeed) > max):
+		return maxSpeed * inputDir
+	return currentSpeed
+	
