7d517685edc2dd1c55969e0546e6961b35103c38
cameronh6w <cgch6w@umsystem.edu>
2026-02-21 17:09:51 -0600

    added tile ground things

--- Scripts/end_level_door.gd
+++ Scripts/end_level_door.gd
@@ -4,6 +4,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+
 	if nextScene == null:
 		push_error("EndLevelDoor has no nextScene!")
 		$AnimatedSprite2D.play("Closed")
