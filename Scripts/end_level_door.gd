926b5f39a51964d5fe546872013711ccf94c96d6
cameronh6w <cgch6w@umsystem.edu>
2026-02-21 23:19:44 -0600

    Doors take to next level and level 2 almost done

--- Scripts/end_level_door.gd
+++ Scripts/end_level_door.gd
@@ -2,7 +2,8 @@ extends LightObject
 class_name EndLevelDoor
 
 @export var canOpen: bool
-@export var nextScene: PackedScene
+#next scene is the string name of  the scene
+@export var nextScene: String
 
 @onready var future_sprite: AnimatedSprite2D = $FutureSprite
 @onready var present_sprite: AnimatedSprite2D = $PresentSprite
@@ -16,15 +17,15 @@ func _ready() -> void:
 	if native_timeline == Timeline.FUTURE:
 		future_sprite.show()
 		active_sprite = future_sprite
-		print("My active sprite:" + str(present_sprite.name))
-		
+		print("My active sprite:" + str(active_sprite.name))
+		active_sprite.play("Closed")
 		present_sprite.hide()
 	else:
 		future_sprite.hide()
 		
 		present_sprite.show()
 		active_sprite = present_sprite
-		print("My active sprite:" + str(present_sprite.name))
+		print("My active sprite:" + str(active_sprite.name))
 
 	if nextScene == null:
 		push_error("EndLevelDoor has no nextScene!")
@@ -44,4 +45,8 @@ func _on_interactable_component_interacted() -> void:
 	active_sprite.play("OpenDoor")
 	await get_tree().create_timer(.3).timeout
 	active_sprite.play("Opened")
+	await get_tree().create_timer(.2).timeout
+	
+	#next scene is the string name of  the scene
+	get_tree().change_scene_to_file("res://Scenes/Levels/"+nextScene+".tscn")
 	
