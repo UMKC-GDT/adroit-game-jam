f26f5a54ca6f27ad2710a1c121245a54ffdf26ba
brandonLaing <38956313+brandonLaing@users.noreply.github.com>
2026-02-22 12:39:41 -0600

    DeathPlanes

--- Scripts/kill_zone.gd
+++ Scripts/kill_zone.gd
@@ -1,5 +1,6 @@
 extends Area2D
 
+@export var currentScene: String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,4 +15,8 @@ func _on_body_entered(body: Node2D) -> void:
 	if (body.is_in_group("Player")):
 		body.kill()
 		#SHOULD WAIT BRIEFLY AND DISPLAY A MESSAGE
-		get_tree().reload_current_scene()
+	var sceneManager:game_manager = get_tree().root.get_node("GameManager")
+	if (sceneManager != null):
+		sceneManager.LoadNewScene("res://Scenes/Levels/"+currentScene+".tscn")
+	else:
+			get_tree().change_scene_to_file("res://Scenes/Levels/"+currentScene+".tscn")
