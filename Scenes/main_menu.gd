b31240628ff2684f05ac799a8aad11330ae51bc5
erikeet <eschmidt359@gmail.com>
2026-02-22 14:05:29 -0600

    Title music and lamp sfx

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -1,16 +1,13 @@
 extends Node2D
 
+var gm: game_manager
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
+	gm = get_tree().root.get_node("GameManager")
 
 func _on_start_pressed() -> void:
+	gm.TitleMusicOutie()
 	var sceneManager:game_manager = find_parent("GameManager")
 	if (sceneManager != null):
 		sceneManager.LoadNewScene("res://Scenes/Levels/Level1.tscn")
