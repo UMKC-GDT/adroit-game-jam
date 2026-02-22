cef16c2243e270ad6a517fc2706dca0c04e3fb69
Zetaquote <popdude36@gmail.com>
2026-02-22 15:19:45 -0600

    Message  Scale

--- Scenes/ui_message.gd
+++ Scenes/ui_message.gd
@@ -7,10 +7,10 @@ extends Node2D
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	$Sprite2D.texture = image
-	$Sprite2D.scale = Vector2(0.75,0.0)
+	$Sprite2D.scale = Vector2(1,0.0)
 	print("Test")
 	var tween = get_tree().create_tween()
-	tween.tween_property($Sprite2D, "scale", Vector2(0.75,0.75), .2) # Replace with function body.
+	tween.tween_property($Sprite2D, "scale", Vector2(1,1), .2) # Replace with function body.
 	tween.tween_property($Sprite2D, "modulate:a", 1.0, 1) # Replace with function body
 	tween.tween_property($Sprite2D, "modulate:a", 0.0, 1.5) # Replace with function body.
 	
