a3f5d096a120f536ddfd85c02df8acd664140757
Zetaquote <popdude36@gmail.com>
2026-02-22 01:17:15 -0600

    Cat 3

---
+++ Scenes/Levels/animated_sprite_2d3.gd
@@ -0,0 +1,24 @@
+extends AnimatedSprite2D
+var catBeMeowin = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	self.play("catMeow")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	pass
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if catBeMeowin == true:
+		self.play("catRun")
+		catBeMeowin = false
+		self.scale.x = -2.0
+		while self.position.x <= 300:
+			self.position.x = self.position.x + 1
+			await get_tree().create_timer(0.01).timeout
+	pass # Replace with function body.
