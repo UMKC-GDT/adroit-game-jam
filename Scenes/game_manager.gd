a7765465a0ef85bf4d6b081539bbffe95aa1804d
brandonLaing <38956313+brandonLaing@users.noreply.github.com>
2026-02-22 12:14:36 -0600

    SceneManagerKindaWorking

--- Scenes/game_manager.gd
+++ Scenes/game_manager.gd
@@ -2,6 +2,7 @@ extends Node2D
 class_name  game_manager
 
 var currentScene
+var currentSceneName
 var mainMenuScene = preload("res://Scenes/MainMenu.tscn")
 @export var ScreenFadeManager: Node
 var isMainMenu: bool = true
@@ -13,20 +14,32 @@ func _ready() -> void:
 	#$SubViewportContainer.visible = false
 	LoadNewScene("res://Scenes/MainMenu.tscn")
 
+var thing:bool = true
 func LoadNewScene(name:String):
-	if name == "res://Scenes/MainMenu.tscn":
-		$SubViewportContainer.visible = false
-		background.hide()
-	else:
-		$SubViewportContainer.visible = true
-		background.show()
-		
-	#$SubViewportContainer/SubViewport.remove_child(currentScene)
 	var newScene:PackedScene = load(name)
 	if (newScene):
-		self.remove_child(currentScene)
+		if (currentScene):
+			if thing:
+				thing= false
+				self.remove_child(currentScene)
+			else:
+				$SubViewportContainer/SubViewport.remove_child(currentScene)
+		
 		currentScene = newScene.instantiate()
-		self.add_child(currentScene)
+		currentSceneName = name
+
+		if name == "res://Scenes/MainMenu.tscn":
+			$SubViewportContainer.visible = false
+			background.hide()
+			self.add_child(currentScene)
+			
+		else:
+			$SubViewportContainer.visible = true
+			background.show()
+			$SubViewportContainer/SubViewport.add_child(currentScene)
+		
+	#$SubViewportContainer/SubViewport.remove_child(currentScene)
+
 	#$SubViewportContainer/SubViewport.add_child(currentScene)
 	#ScreenFadeManager.fadeOut()
 	
