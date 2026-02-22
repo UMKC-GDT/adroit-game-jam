c7c9057e8a99845054997c78c33b3784342fc422
cameronh6w <cgch6w@umsystem.edu>
2026-02-21 20:50:01 -0600

    level 2  light flicker works

--- Assets/flickering_lamp.gd
+++ Assets/flickering_lamp.gd
@@ -3,16 +3,39 @@ class_name FlickeringLight
 
 @onready var beam: QuantumLight = $QuantumLight
 
+var lightOnPresent  = false
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var time = 0.0;
 func _process(delta: float) -> void:
-	beam.timeline_type = beam.Timeline.FUTURE;
-	$Sprite2D.visible= false
-	await get_tree().create_timer(5).timeout
-	beam.timeline_type = beam.Timeline.PRESENT;
-	$Sprite2D.visible= true
-	await get_tree().create_timer(5).timeout
+	time+=delta
+	if time>=0.1 and time <=0.2:
+		
+		if(lightOnPresent):
+			beam.timeline_type = beam.Timeline.PRESENT
+			beam.update_light()
+			lightOnPresent=false
+			
+		else:
+			beam.timeline_type = beam.Timeline.FUTURE
+			beam.update_light()
+			lightOnPresent=true
+	
+	if time>=1.5:
+		time = 0.0
+		if(lightOnPresent):
+			beam.timeline_type = beam.Timeline.PRESENT
+			beam.update_light()
+			lightOnPresent=false
+			
+		else:
+			beam.timeline_type = beam.Timeline.FUTURE
+			beam.update_light()
+			lightOnPresent=true
+
+
+	
 	
