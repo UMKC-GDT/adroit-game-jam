0fa0f9283c0b2ee1a72c3f9d73a9cbbbecd565a7
Trevor Norton <90813576+nortrev02@users.noreply.github.com>
2026-04-24 13:33:20 -0500

    Very Basic Score System
    ngl haven't changed too much

--- Scripts/scoring_system.gd
+++ Scripts/scoring_system.gd
@@ -1,6 +1,12 @@
 extends Control
+# Started by Trevor
+#TODO: rest of the fucking system
 
+#Cat Counter
 var collectedCats: int = 0
+# Star Time: Finish the level before this many seconds have passed to recieve a star.
+var starTime: float
+var starTimeList: Array[float] = [0, 0, 0, 0, 0, 5.65]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +17,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# Designed to be called by the cats whenever they are discovered.
 func cat_collected():
 	collectedCats += 1
 	$CatLabel.text = ("Cats Found: " + str(collectedCats))
 	
+# Designed to be called by the game manager whenever a new level is loaded.
+func changeLevel(newLevel: int):
+	$LevelLabel.text = ("Level " + str(newLevel))
+	starTime = starTimeList[newLevel - 1]
